fd93fcefa35db18e1cb33cfb068207bdd1925cb5
javahometech <kammana.hari@gmail.com>
2019-05-28 14:30:42 +0530

    Update Calculator.groovy

--- src/in/javahome/Calculator.groovy
+++ src/in/javahome/Calculator.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
 
 package in.javahome
-
-def add(x,y){
-  println("This is add method")
-  def r = x+y
-  return r
+public class Calculator{
+  def add(x,y){
+    println("This is add method")
+    def r = x+y
+    return r
+  }
 }
